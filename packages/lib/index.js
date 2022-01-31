d015fd33a951c7900e16136b2057d9c027504a16
Matej Lednicky <matej.lednicky@typeform.com>
2022-01-31 17:43:44 +0100

    feat: button text

--- packages/lib/index.js
+++ packages/lib/index.js
@@ -1,5 +1,5 @@
 
-export const defaultText = 'click this button here'
+export const defaultText = 'click this button here right now!'
 
 export const defaultOnClick = () => {console.log('you clicked the button')}
 
