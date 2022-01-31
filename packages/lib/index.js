1e06d313bfeedf6cf3831ac4efc8f11fa38b7f93
Matej Lednicky <matej.lednicky@typeform.com>
2022-01-31 14:28:04 +0100

    feat: vanilla lib

---
+++ packages/lib/index.js
@@ -0,0 +1,14 @@
+
+const button = (
+  text = 'click me',
+  onclick = () => {console.log('you clicked the button')},
+  parent = document.body
+) => {
+  const btn = document.createElement('button')
+  btn.innerHTML = text
+  btn.onclick = onclick
+  console.log('parent', parent)
+  parent.appendChild(btn)
+}
+
+window.button = button
