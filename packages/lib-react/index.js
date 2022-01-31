467703c1ff5eea5e71490e0cfaf32d9efd5288e6
Matej Lednicky <matej.lednicky@typeform.com>
2022-01-31 17:07:49 +0100

    feat: update react

--- packages/lib-react/index.js
+++ packages/lib-react/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {defaultOnClick, defaultText} from '@mathio28/lib'
 
+// button component
 export const Button = ({ onClick = defaultOnClick, children = defaultText }) => (
   <button onClick={onClick}>{children}</button>
 )
