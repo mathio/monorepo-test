7aa2b62360dd8a5b28b9d04e4d01eb033cb23dd4
Matej Lednicky <matej.lednicky@typeform.com>
2022-01-31 17:23:55 +0100

    fix: react

--- packages/lib-react/index.js
+++ packages/lib-react/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {defaultOnClick, defaultText} from '@mathio28/lib'
 
-// main button component
+// main button component, exported
 export const Button = ({ onClick = defaultOnClick, children = defaultText }) => (
   <button onClick={onClick}>{children}</button>
 )
